cf9c985437d796a9b2924d1d4e58fb6efd342282
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2018-04-08 19:23:08 +0300

    explicit import from secret file

--- openvpnscale/settings.py
+++ openvpnscale/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from .settings_secret import *
+from .settings_secret import (
+    ALLOWED_HOSTS,
+    SECRET_KEY
+)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,14 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
-
 
 # Application definition
 
